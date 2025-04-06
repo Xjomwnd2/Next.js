d5df162c3cc062d7501d7530dab35a3197b6b0ab
YourGitHubUsername <jmwaura@byupathway.edu>
2025-04-06 10:51:41 +0300

    commit

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,6 +1,3 @@
-import { Lusitana } from 'next/font/google';
-
-export const lusitana = Lusitana({
-  weight: ['400', '700'], // or just '400' if you're only using regular
-  subsets: ['latin'],
-});
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
